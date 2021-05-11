0d2fee3d5c6554b1eb3288a20efb114da3132e28
ArturSantiagoFBiague <70341253+ArturSantiagoFBiague@users.noreply.github.com>
2021-05-11 13:45:08 -0300

    Criando classes: Golpes e Monstros :construction: :
    Criando super class Monstros e seu get/set:
    Criando a classe golpes;

---
+++ copiamon/src/main/java/br/sp/uam/poo/copiamon/Golpes.java
@@ -0,0 +1,49 @@
+
+package br.sp.uam.poo.copiamon;
+
+
+public class Golpes {
+    private String nome;
+    private String tipo;
+    private int poder;
+    
+    
+    public Golpes(String nome, String tipo, int poder){
+        setNome(nome);
+        setTipo(tipo);
+        setPoder(poder);
+
+    }
+
+    
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setPoder(int poder) {
+        this.poder = poder;
+    }
+
+    
+    
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public int getPoder() {
+        return poder;
+    }
+    
+}
+
+
---
+++ copiamon/src/main/java/br/sp/uam/poo/copiamon/Monstros.java
@@ -0,0 +1,18 @@
+
+package br.sp.uam.poo.copiamon;
+
+
+
+public class Monstros {
+    
+    private Golpes golpe1;
+    private String nome;
+    private String elemento;
+    private int vida;
+    private int defesa;
+    private int velocidade;
+    
+     
+    
+    
+}
